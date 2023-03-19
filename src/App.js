39378a408ad95fe25ea91aa117eab43c726c208f
gizem sangur <gizem2002sangur@gmail.com>
2023-03-19 15:03:17 +0300

    Terminal, Hata listeleme, login sayfaları oluşturuldu.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Terminals from "./Components/Terminals";
+import Login from "./Components/Login";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Terminals />} />
+        <Route path="/Login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Components/Terminals.js
@@ -0,0 +1,103 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { AppBar, Badge, Button, Grid } from "@mui/material";
+import { Container } from "@mui/material";
+import { Toolbar } from "@mui/material";
+import { Box } from "@mui/system";
+import { Typography } from "@mui/material";
+import {Routes, Route, useNavigate} from 'react-router-dom';
+
+ function Terminals() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/data")
+      .then((res) => {
+        console.log(res.data);
+        setData(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const navigate = useNavigate();
+
+  const navigateToLogin = () => {
+    
+    navigate('/Login');
+  };
+
+  const arr = data.map((data, index) => {
+    return (
+      <Grid container direction="row" sx={{ lineHeight: 1.5 }}>
+        <Grid item xs={2} sx={{ borderRight: 1, borderBottom: 1 }}>
+          <Typography sx={{ padding: 1 }}>{data.depName}</Typography>
+        </Grid>
+        <Grid item xs={10} sx={{ borderBottom: 1, textAlign: "left" }}>
+          {data.filterBaseds.map((subitem, i) => {
+            return (
+            
+                <Button  onClick={navigateToLogin}
+                  sx={{ border: 1, fontSize: 14, color: "black", margin: 1 }}>
+                  {subitem.filterCode}
+                </Button>
+                
+            );
+          })}
+        </Grid>
+      </Grid>
+    );
+  });
+
+  return (
+    <div>
+      <Grid>
+        <AppBar position="static">
+          <Container maxWidth="xl">
+            <Toolbar disableGutters>
+              <Typography
+                variant="h6"
+                noWrap
+                component="a"
+                href="/"
+                sx={{
+                  mr: 2,
+                  display: { xs: "none", md: "flex" },
+                  fontFamily: "monospace",
+                  fontWeight: 700,
+                  letterSpacing: ".3rem",
+                  color: "inherit",
+                  textDecoration: "none",
+                }}
+              >
+                Complete Vehicle Quality
+              </Typography>
+              <Box
+                sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
+              ></Box>
+            </Toolbar>
+          </Container>
+        </AppBar>
+      </Grid>
+
+      <Grid container sx={{ mt: 5, border: 1, borderRadius: 2 }}>
+        <Grid container direction="row">
+          <Grid item xs={12}>
+            <Typography sx={{ borderBottom: 1 }}>Tum Terminaller</Typography>
+          </Grid>
+        </Grid>
+        <Grid container direction="row">
+          <Grid item xs={2}>
+            <Typography sx={{ borderRight: 1, borderBottom: 1 }}>
+              Bolum Bazinda
+            </Typography>
+          </Grid>
+          <Grid item xs={10}>
+            <Typography sx={{ borderBottom: 1 }}>Filtre Bazinda</Typography>
+          </Grid>
+        </Grid>
+        {arr}
+      </Grid>
+    </div>
+  );
+}
+export default Terminals;
